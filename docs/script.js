00983982d838abde354769b5268717cb4b05e2df
René <shiome80@gmail.com>
2019-11-11 14:23:17 +0100

    Added all the things
    Added multiple sections
    Added Navigation bar on all pages
    Moved stuff to correct pages
    Button function in javascript to hide pics

--- docs/script.js
+++ docs/script.js
@@ -2,3 +2,12 @@ function buttonClicked(){
     //console.log("Click!");
     document.getElementById("button").innerHTML = "Clicked";
 }
+
+function hideFunc() {
+    var x = document.getElementById("obiwan");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
