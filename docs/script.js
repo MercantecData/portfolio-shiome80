b4e1062cf44a4917e92d439a5ea8384d31d04b29
René <shiome80@gmail.com>
2019-11-13 12:57:17 +0100

    Update
    JavaScript assignment done
    CSS Grid assignment done
    Minor changes :::
    Footer added
    Custom form now prints input without deleting previous
    General cleanup
    Many comments in .js file

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,4 @@
-function buttonClicked(){
-    //console.log("Click!");
-    document.getElementById("button").innerHTML = "Clicked";
-}
-
+//Hides Obiwan
 function hideFunc() {
     var x = document.getElementById("obiwan");
     if (x.style.display === "block") {
@@ -12,21 +8,55 @@ function hideFunc() {
     }
 }
 
+//Changes form to only single string
 function changeText() {
     document.getElementById("form2").innerHTML = "ALL MALE";
 }
 
+//Removes form 2
 function removeSomething() {
     document.getElementById("form2").remove();
 }
 
+//Changes background color on <div> button
 function myOwnFunction (button) {
     button.style.backgroundColor = "white" ;
 }
 
+//Adds a new paragraph with content from custom form
+function addP() {
+    var firstname = document.getElementById("customFirst").value;
+    var lastname = document.getElementById("customLast").value;
+    var fullname = firstname + " " + lastname;
+    var newP = "<p>" + fullname + "</p>";
+    document.getElementById("blankSpace").insertAdjacentHTML("afterbegin", newP);
+}
+
+//Just for flavor - Clears guestbook writings
+function clearPrint() {
+    document.getElementById("blankSpace").innerHTML = "";
+}
+
+
+
+
+//Old solution to print guestbook assignment
+//
+/*
 function printContent() {
     var firstname = document.getElementById("customFirst").value;
     var lastname = document.getElementById("customLast").value;
     var fullname = firstname + " " + lastname;
-    document.getElementById("fullname").innerHTML = fullname;
+
+    document.getElementById("blankSpace").innerHTML += fullname + "<br>";
 }
+*/
+
+//Solution to another student problem
+/*
+function buttonClicked(){
+    const x = document.getElementById("fullname");
+    x.style.fontFamily = "Courier new";
+    console.log(x.style.fontFamily);
+}
+*/
